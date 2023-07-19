cd4ff85efe3aaeec6a0b53080472ab1c87765641
David <doladepo128@gmail.com>
2023-07-19 19:03:17 +0100

    Installed react-slick library for image-slider

--- src/components/ImgSlider.jsx
+++ src/components/ImgSlider.jsx
@@ -1,7 +1,43 @@
 import styled from 'styled-components';
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import Slider from "react-slick";
+
+
 
 const ImgSlider = (props) => {
-  
+  let settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+  };
+  return (
+      <Carousel {...settings}>
+        <div>
+            <h3>8667</h3>
+        </div>
+        </Carousel>  
+  )  
 }
+
+const Carousel = styled(Slider)`
+  margin-top: 40px;
+ 
+  & > button {
+    opacity: 0;
+    height: 100%;
+    width: 5vw;
+    z-index: 1;
+  }
+
+  &:hover{
+    opacity: 1;
+    transition: opacity 0.2s ease;
+  }
+`;
+
 
 export default ImgSlider;
