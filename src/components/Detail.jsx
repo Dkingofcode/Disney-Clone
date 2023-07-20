554e73f56acf70f9a900323cfbd89d53ed00302c
David <doladepo128@gmail.com>
2023-07-20 12:13:47 +0100

    Working on the movie detail page

--- src/components/Detail.jsx
+++ src/components/Detail.jsx
@@ -1,8 +1,36 @@
 import styled from 'styled-components';
-
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import db from '../firebase';
 
 const Detail = (props) => {
-   return (
+    const { id } = useParams();
+    const [ detail, setDetail] = useState([]);
+   
+    useEffect(() => {
+        const fetchData = async () => {
+          try {
+            const docRef = doc(db, "movies", id);
+            const docSnap = await getDoc(docRef);
+      
+            if (docSnap.exists()) {
+              setDetail(docSnap.data());
+              console.log("Document data:", docSnap.data());
+            } else {
+              // docSnap.data() will be undefined in this case
+              console.log("No such document!");
+            }
+          } catch (error) {
+            // Handle the error here
+            console.error("Error fetching data:", error);
+          }
+        };
+      
+        fetchData();
+      }, [id]);
+      
+
+    return (
      <Container>
         <Background>
             <img alt="" src="/images/boliviaunsplash.jpg"  />
@@ -11,10 +39,37 @@ const Detail = (props) => {
          <ImageTitle>
            <img alt="" src="/images/bunsplash.jpg" />
          </ImageTitle>
+         <ContentMeta>
+            <Control>
+               <Player>
+                  <img src='/images/play-icon-black.png' alt="" />
+                  <span>Play</span>
+                </Player>
+                <Trailer>
+                  <img src='/images/play-icon-white.png' alt=""  />  
+                  <span>Trailer</span>
+                </Trailer>
+                <AddList>
+                    <span>
+                            
+                    </span>
+                </AddList>
+                <GroupMatch>
+                  <div>
+                    <img src="/images/group-icon.png" alt=""  />  
+                  </div>   
+                </GroupMatch>
+            </Control>
+            <SubTitle>
+             <div>Subtitle</div>   
+            </SubTitle>
+            <Description>
+              <div>Description</div>
+            </Description>
+         </ContentMeta>
      </Container>
    );  
-
-   }
+};
 
   const Container = styled.div`
   position: relative;
@@ -45,6 +100,86 @@ const Detail = (props) => {
   }
   `;
 
+     const Trailer = styled(Player)`
+      background: rgba(0, 0, 0, 0.3);
+      border: 1px solid rgb(249, 249, 249);
+      color: rgb(249, 249, 249);        
+     `;
+
+     const AddList = styled.div`
+       margin-right: 16px;
+       height: 44px;
+       width: 44px;
+       display: flex;
+       justify-content: center;
+       align-items: center;
+       background-color: rgba(0, 0, 0, 0.6);
+       border-radius: 50%;
+       border: 2px solid white;
+       cursor: pointer;
+       
+       
+       span{
+        background-color: rgb(249, 249, 249,);
+        display: inline-block;
+
+        &:firts-child{
+            height: 2px;
+            transform: translate(1px, 0px) rotate(0deg);
+            width: 16px;
+        }
+
+        &:nth-child(2){
+            height: 16px;
+            transform: translate(-8px) rotate(0deg);
+            width: 2px;
+        }
+       }
+     `;
+
+     const GroupMatch = styled.div`
+     height: 44px;
+     width: 44px;
+     display: flex;
+     justify-content: center;
+     align-items: center;
+     background-color: white;
+     border-radius: 50%;
+     cursor: pointer;
+     
+     div{
+        height: 40px;
+        width: 40px;
+        background: rgb(0, 0, 0, 0.5);
+        border-radius: 50%;
+
+        img{
+            width: 100%;
+        }
+     }
+
+     `;
+
+     const SubTitle = styled.div`
+       color: rgb(249, 249, 249);
+       font-size: 15px;
+       min-height: 20px;
+
+       @media (max-width: 768px){
+        font-size: 12px;
+       }
+     `;
+
+     const Description = styled.div`
+       line-height: 1.4;
+       font-size: 20px;
+       padding: 16px 0px;
+       color: rgb(249, 249, 249);
+
+       @media (max-width: 768px){
+        font-size: 14px;
+       }
+     `;
 
 
 export default Detail;
